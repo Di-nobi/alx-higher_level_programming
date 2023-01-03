c3e3106a083a8e21d50d09f8aa10562d46f2ce19
Dinobi <architectstan01@gmail.com>
2023-01-02 16:05:13 -0800

    upsd

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,17 +2,15 @@
 #define LISTS_H
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure 
+ * for Holberton school
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -23,4 +21,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head,const int n);
 void free_listint(listint_t *list);
 int check_cycle(listint_t *list);
-#endif
+
+#endif /* LISTS_H */
