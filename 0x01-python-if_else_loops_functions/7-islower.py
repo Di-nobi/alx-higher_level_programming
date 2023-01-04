5b9340a1dbe67c691f456d6064181f50f88f5973
Dinobi <architectstan01@gmail.com>
2023-01-04 11:23:41 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == ord(97) and c == ord(122):
+    if ord(c) == ord(97) and ord(c) == ord(122):
         return True
-    elif c == ord(65) and c == ord(90):
+    elif ord(c) == ord(65) and ord(c) == ord(90):
         return False
