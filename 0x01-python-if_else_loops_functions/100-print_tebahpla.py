2d5b63e02c7ffbb6335d8b9a3d60958f8780e133
Dinobi <architectstan01@gmail.com>
2023-01-04 11:03:36 -0800

    teba

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range (97, 123, -1):
+for i in range (122, 96, -1):
     if i % 2 == 0:
         i = i - 32
-        print("{:c}".format(i), end="")
-    else:
-        print("{}".format(i), end="")
+        print("{:c}".format(i), end=' ')
