f263c8cf1776efbf2561a49f5ab4f5ab481d1553
Dinobi <architectstan01@gmail.com>
2023-01-05 10:52:19 -0800

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+n = len(sys.argv) - 1
+
+if n == 0:
+    print("{} arguments.".format(n))
+elif n == 1:
+    print("{} argument:".format(n))
+else:
+    print("{} arguments:".format(n))
+    for i in range(1, n):
+        if i != 0:
+            print("{} argument:".format(i))
+            i += 1
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def func(nargs):
+        if nargs % 3
