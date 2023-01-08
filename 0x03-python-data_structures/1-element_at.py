c6d2d544c54418abdf8235b4e787d8eea43f6a6c
Dinobi <architectstan01@gmail.com>
2023-01-08 09:13:59 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     if idx == -1:
         return None
-    elif idx > my_list:
+    elif idx > len(my_list):
         return None
-    print("Elements at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    else:
+        return my_list[idx]
