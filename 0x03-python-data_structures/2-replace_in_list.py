a9c1775af0caac645d5938713db54a8baf735b20
Dinobi <architectstan01@gmail.com>
2023-01-11 00:20:45 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list[idx]
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return my_list[idx]
     else:
         my_list[idx] = element
+        return my_list = element
