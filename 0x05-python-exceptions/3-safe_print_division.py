54b52daecd2e6474085117a2118bc031fb068b39
Dinobi <architectstan01@gmail.com>
2023-01-23 20:02:41 -0800

    div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div= a / b
-        print(div)
+        div = a / b
     except ZeroDivisionError:
         div = 0
     except TypeError:
         div = 0
     finally:
         print("Inside result: {}".format(div))
+    return (div)
