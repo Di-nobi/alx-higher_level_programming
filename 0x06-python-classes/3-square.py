1b36a2abaa57b08eb81f25ff9da7067a444e4c7b
Dinobi <architectstan01@gmail.com>
2023-01-24 10:25:06 -0800

    3-sqre

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+#By Dinobi
+"""A module that defines a square """
 
 class Square:
+    """A class that represents a square"""
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
 
         if not int(size):
             raise TypeError('size must be an integer')
