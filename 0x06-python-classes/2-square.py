7d745c2a8920b6eb16f70a84434f1bc874f2c0fd
Dinobi <architectstan01@gmail.com>
2023-01-24 10:09:22 -0800

    2-sqre

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,6 +2,13 @@
 
 class Square:
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
