2c42300cb39bdcb8576a574b427edc0508298e84
Dinobi <architectstan01@gmail.com>
2023-01-31 07:17:01 -0800

    5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,6 +15,7 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
     @property
     def size(self):
         return(self.__size)
@@ -23,7 +24,12 @@ class Square:
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
     def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
         return (self.__size ** 2)
     def my_print(self):
         for i in range(self.__size):
