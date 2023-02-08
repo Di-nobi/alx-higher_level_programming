692697767cdad3d0dcf4404f9b97910d65f045d1
Dinobi <architectstan01@gmail.com>
2023-02-07 20:32:40 -0800

    i/o

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module that contains a function that appends from a file """
+def append_write(filename="", text=""):
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+    with open('filename', 'a', encoding="utf-8") as f:
+        append_file = f.append(text)
+        return append_file
