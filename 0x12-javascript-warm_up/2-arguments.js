fd31fa318153e03c9107b70cc22748f6b5f94a93
Dinobi <architectstan01@gmail.com>
2023-04-11 09:50:53 -0700

    Javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-//a script that prints a message depending of the number of arguments passed
-if (process.argv.length === 2)
-{
-	console.log('No argument');
-}
-else if (process.argv.length === 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Argument found');
-}
+//	A script that prints a message
+const find = process.argv.length;
+console.log(find === 2 ? 'No argument' : find === 3 ? 'Argument found' : 'Arguments found');
