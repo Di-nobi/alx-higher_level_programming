517219578e9b74e62c927aced010f05ccbe03d46
Dinobi <architectstan01@gmail.com>
2023-04-12 17:47:24 -0700

    Javascript object and scopes

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const filepath = require('filepath');
-const first = filepath.readFileSync(process.argv[2], 'utf8');
-const second = filepath.readFileSync(process.argv[3], 'utf8');
-filepath.writeFileSync(process.argv[4], first + second);
+const fs = require('fs');
+const first = fs.readFileSync(process.argv[2], 'utf8');
+const second = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], first + second);
