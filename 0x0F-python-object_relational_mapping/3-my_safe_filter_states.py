b8ff4004d5145f15db691f6cfbc973365916a43e
Dinobi <architectstan01@gmail.com>
2023-04-15 00:13:00 -0700

    OBJECT RELATIONAL MAPPING

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument
+"""
+if __name__ == 'main':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db= sys.argv[3])
+    cur = db.cursor()
+    match =sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
