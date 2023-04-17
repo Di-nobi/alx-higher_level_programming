10bdbb197cab3c18b8e3bcc7817758afdfbaaa54
Dinobi <architectstan01@gmail.com>
2023-04-17 14:07:41 -0700

    OBJECT MAPPING

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,6 +11,8 @@ if __name__ == "__miain__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     session = Session(engine)
+    new_state = State(name='Louisiana')
+    session.add(new_state)
     state = session.query(State).filter_by(name='Louisiana').first()
     print(state)
     session.commit()
