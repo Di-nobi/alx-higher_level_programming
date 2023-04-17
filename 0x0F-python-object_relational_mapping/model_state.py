1c3bef721bb15b0f42c3ec9507c870b65eaac067
Dinobi <architectstan01@gmail.com>
2023-04-17 11:58:15 -0700

    OBJECT MAPPING

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
-Start link class to table in database
+Stores an instance od declarative_base()
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-            Base.metadata.create_all(engine)
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+storemetadata = MetaData()
+Base = declarative_base(metadata=storemetadata)
+
+class State(Base):
+    """
+    id and name attributes of each state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
