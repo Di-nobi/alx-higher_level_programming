804ac4ee48108aafb96ff7436d24a2a5116fd4c4
Dinobi <architectstan01@gmail.com>
2023-04-17 13:58:47 -0700

    OBJECT MAPPING

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a script that adds the State object “Louisiana” to the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+i
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(state)
+    session.commit()
