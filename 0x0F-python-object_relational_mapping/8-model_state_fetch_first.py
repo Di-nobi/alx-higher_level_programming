3daac5900ba901cbe6ec06bb459f5bd9344f266b
Dinobi <architectstan01@gmail.com>
2023-04-17 10:59:12 -0700

    OBJECT RELATIONAL MAPPING

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
+    import sys
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
