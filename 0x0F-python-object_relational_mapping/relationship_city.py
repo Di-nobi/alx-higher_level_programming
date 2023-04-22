b6f948170d1b58d28f4750590b433011da8dc207
Dinobi <architectstan01@gmail.com>
2023-04-22 04:30:27 -0700

    OBJECT MAPPING

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 from relationship_state import Base
 
 class City(Base):
-    __tablename__ = "cities"
+    __tablename__ = 'cities'
 
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
