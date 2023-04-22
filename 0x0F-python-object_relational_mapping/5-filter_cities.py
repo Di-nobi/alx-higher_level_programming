b2c23caa72bbb210a859173aac0be93856578fd1
Dinobi <architectstan01@gmail.com>
2023-04-22 03:37:59 -0700

    OBJECT MAPPING

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ lists all cities of the state, using the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port = 3306, usr = sys.argv[1], passwd = [2], db = sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port = 3306, usr = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT cities.name, cities.id FROM 
                 cities JOIN states ON cities.state_id = states.id
