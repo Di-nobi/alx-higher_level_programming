f4376beee60e831221f1255b31e4f90afb9c8f74
Dinobi <architectstan01@gmail.com>
2023-04-22 03:54:35 -0700

    OBJECT MAPPING

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
                 WHERE states.name = %s
                 ORDER BY cities.id ASC""", (sys.argv[4],))
     rows = cur.fetchall()
-    for row in rows:
-        print("{}, ".format(list(row[0])))
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
