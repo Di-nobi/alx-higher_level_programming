cb8672465de7ebaff3c3def58df9cac8cebca1f6
Dinobi <architectstan01@gmail.com>
2023-04-28 23:41:48 +0000

    Python Network

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    res = requests.post('http://0.0.0.0:5000', data={'q': letter})
+    try:
+        respond = r.json()
+        if respond == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(respond.get("id"), respond.get("name")
+    except ValueError:
+        print("Not a valid JSON")
