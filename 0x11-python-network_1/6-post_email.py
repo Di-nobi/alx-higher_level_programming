b20c5fefd3f6ad134dd87271e460ffb1c0d31baa
Dinobi <architectstan01@gmail.com>
2023-04-29 08:11:11 +0000

    Python Network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import requests
 import sys
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]})
+    value = {"email": sys.argv[2]}
     respond = requests.POST(url, data=value)
     print(respond.text)
