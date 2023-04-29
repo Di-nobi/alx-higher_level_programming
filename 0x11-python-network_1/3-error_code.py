5036a0254c5681f0ca93fbb285c245e308b84e34
Dinobi <architectstan01@gmail.com>
2023-04-29 08:20:33 +0000

    Python Network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL 
-and displays the body 
-of the response (decoded in utf-8)"""
+"""Python script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body
+"""
 
 import sys
 from urllib import request, error
