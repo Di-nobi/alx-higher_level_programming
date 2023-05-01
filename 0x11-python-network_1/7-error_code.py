14cb78de2e6a54af2b6785c6da3011cc984ebc8c
Dinobi <architectstan01@gmail.com>
2023-05-01 14:39:06 -0700

    $message

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,14 +2,13 @@
 """ A python script that checks for the error code"""
 
 import sys
-from urllib import  requests
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     res = requests.get(url)
-    try:
-        if res.status_code >= 400:
-            print('Error code: {}'.format(res.status_code))
-        else:
-            print(res.text)
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
+    else:
+        print(res.text)
