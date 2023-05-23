51faf6a9923232c00bd5fa8557b1dc3d856295b3
Dinobi <architectstan01@gmail.com>
2023-05-23 15:44:18 -0700

    Web Scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 request.get(process.argv[2], function (err, response, body) {
-   if (!err) {
-     fs.writeFileSync(process.argv[3], body);
+   if (err) {
+     console.log(err);
    }
+   fs.writeFileSync(process.argv[3], body, 'utf-8', (err) => {
+     if (err) {
+       console.log(err);
+     }
+   });
 });
