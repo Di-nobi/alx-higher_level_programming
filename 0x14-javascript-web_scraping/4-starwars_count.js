dec82136680c949b710bf81a401b58cc3535a0a5
Dinobi <architectstan01@gmail.com>
2023-05-23 18:35:26 -0700

    Starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
-const req = require('req');
-req.get(process.argv[3], function (err, response, body) {
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
     const movies = JSON.parse(body).movies;
     let num = 0;
-     const film;
+     for (const movie of movies) {
+       for (const move of movie.characters) {
+	 if (move.endswith('/18/')) {
+	   num++;
+	 }
+       }
+     }
+	  console.log(num);
+  });
 
